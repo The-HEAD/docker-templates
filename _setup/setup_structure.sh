e462ad1ea226b2240b7d69f7f0eebd8500a8a640
Timo <43415929+The-HEAD@users.noreply.github.com>
2022-08-30 14:46:00 +0200

    Update setup_structure.sh

--- _setup/setup_structure.sh
+++ _setup/setup_structure.sh
@@ -26,11 +26,14 @@ mkdir -p ${BASE_FOLDER}/custom || error "Failed to create subfolder shared folde
 mkdir -p ${BASE_FOLDER}/logs || error "Failed to create subfolder shared folder!"
 mkdir -p ${BASE_FOLDER}/shared || error "Failed to create subfolder shared folder!"
 mkdir -p ${BASE_FOLDER}/secrets || error "Failed to create subfolder shared folder!"
-
-touch ${BASE_FOLDER}/appdata/test.txt || error "Failed to create traefik route config!"
+echo "${BASE_FOLDER}/secrets created - you should run the following command after finsihing your setup to secure your secrets:"
+echo ">  sudo chown root:root ${BASE_FOLDER}/secrets && sudo chmod 600  ${BASE_FOLDER}/secrets "
 
 echo "Setting rights to ${USER}:${GROUP} again for all files/folders..."
 chown -R ${USER}:${GROUP} ${BASE_FOLDER} || error "Failed to CHOWN"
-chmod -R 755 ${BASE_FOLDER} || error "Failed to CHMOD"
+# chmod -R 775 ${BASE_FOLDER} || error "Failed to CHMOD"
+# better way with different rights for folders and existing files (if they exist)
+sudo find ${BASE_FOLDER} -type d -exec chmod 775 {} + || error "Failed to CHMOD for Directories"
+sudo find ${BASE_FOLDER} -type f -exec chmod 664 {} + || error "Failed to CHMOD for Files"
 
 echo "Setup complete."
